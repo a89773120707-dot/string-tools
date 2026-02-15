2e1f0c39d30a1ef153a5f3b6c53f40917309a5a3
a89773120707-dot <a89773120707@gmail.com>
2026-02-15 10:26:54 +0300

    feat: add-cleanstring-function (#1)
    Co-authored-by: Anar <drop.bot@inbox.ru>
    убирает пробелы по краям, приводит к нижнему регистру, удаляет всё кроме букв и цифр.

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
 
 func main() {
-	fmt.Println("string-tools")
+	fmt.Println("== string-tools ==")
+	fmt.Println("hello, go 'git- traning! ->", CleanString("hello, go 'git- traning! "))
+}
+
+func CleanString(s string) string {
+	s = strings.ToLower(strings.TrimSpace(s))
+
+	var b strings.Builder
+	b.Grow(len(s))
+
+	for _, r := range s {
+		if unicode.IsLetter(r) || unicode.IsDigit(r) || r == ' ' {
+			b.WriteRune(r)
+		}
+	}
+	return b.String()
 }
